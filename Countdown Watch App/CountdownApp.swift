1a3dc5bd3bfda1c293ae34a34e54c3c2714450fc
YunxiJiang <matthewtkd9@gmail.com>
2022-12-20 20:20:25 +0000

    Initial Commit

---
+++ Countdown Watch App/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown Watch App
+//
+//  Created by Yunxi Jiang on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Countdown_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
